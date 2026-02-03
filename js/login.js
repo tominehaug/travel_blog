865a8c5db9b2f357579b79556ae3c354ec8cbbb6
tominehaug <tomine_haug@live.no>
2026-02-03 13:16:47 +0100

    unfinished login/authentication logic

---
+++ js/login.js
@@ -0,0 +1,36 @@
+let isLoggedIn = false;
+
+const loginForm = document.getElementById("login-form");
+loginForm.addEventListener("submit", handleLogin);
+
+async function handleLogin(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const body = {
+    email: formData.get("email"),
+    password: formData.get("pwd"),
+  };
+  // HTTP POST request
+  try {
+    const response = await fetch("https://api.noroff.dev/v2/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    // API checks if user exists
+    const data = await response.json();
+    if (!response.ok)
+      throw new Error(data.errors?.[0]?.message || "Login failed");
+
+    function saveLogin(user) {
+      localStorage.setItem("token", user.accessToken);
+      localStorage.setItem("username", user.name);
+    }
+    saveLogin(data.data);
+    window.location.href = "/index.html";
+  } catch (error) {
+    console.error("login failed" + error);
+    //document.getElementById("loginMessage").textContent = error.message;
+  }
+}
