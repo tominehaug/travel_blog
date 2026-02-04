1ccb3943b95d18e866f3bc8ded90f5af1cf704ae
tominehaug <tomine_haug@live.no>
2026-02-04 15:18:29 +0100

    form validation for registration form

---
+++ js/registration.js
@@ -0,0 +1,54 @@
+const registrationForm = document.getElementById("registration-form");
+registrationForm.addEventListener("submit", validateForm);
+
+async function validateForm(event) {
+  event.preventDefault();
+
+  let isValid = true;
+
+  const inputs = Array.from(registrationForm.querySelectorAll("input"));
+
+  inputs.forEach((input) => {
+    const errorDiv = document.querySelectorAll(".error");
+
+    if (!input.checkValidity()) {
+      errorDiv.textContent = input.validationMessage;
+      isValid = false;
+    } else {
+      errorDiv.textContent = "";
+    }
+  });
+
+  const password = registrationForm.elements.password.value;
+  const confirm = registrationForm.elements.confirmPwd.value;
+  const confirmErrorDiv =
+    registrationForm.elements.confirmPwd.nextElementSibling;
+
+  if (password === confirm) {
+    isValid = true;
+  } else {
+    confirmErrorDiv.textContent = "Passwords don't match";
+  }
+
+  if (isValid) {
+    console.log("Validated!<3333");
+    // handleRegistration();
+  }
+}
+
+// async function handleRegistration(event) {
+//   event.preventDefault();
+
+//   const formData = new FormData(event.target);
+
+//   let pwd = document.getElementById("password");
+//   let rpass = document.getElementById("repeatpass");
+
+//   const body = {
+//     username: formData.get("username"),
+//     email: formData.get("email"),
+//     password: formData.get("password"),
+//   };
+
+//   console.log(body);
+// }
