fed013e75990ce2cb3225e34168166c8bee23280
tominehaug <tomine_haug@live.no>
2026-02-12 11:29:29 +0100

    Made logic for creating and uploading blog posts

--- js/create-post.js
+++ js/create-post.js
@@ -1,3 +1,7 @@
+import { setPopup, showPopup, hidePopup } from "./popup.js";
+const username = localStorage.getItem("username");
+const token = localStorage.getItem("token");
+
 const createForm = document.getElementById("create-form");
 createForm.addEventListener("submit", validateForm);
 
@@ -10,7 +14,7 @@ async function validateForm(event) {
   inputs.forEach((input) => {
     const errorDiv = input.nextElementSibling;
 
-    // validate file input
+    //validate file input
     if (input.type === "file") {
       if (!input.files || input.files.length === 0) {
         errorDiv.textContent = "Please fill out this field";
@@ -40,11 +44,54 @@ async function validateForm(event) {
 }
 
 async function uploadPost() {
+  //Collect blog post data
   const formData = new FormData(createForm);
+  const ingressInput = formData.get("ingress");
+  const bodyInput = formData.get("body");
+  const bodyTxt = `<p class="ingress">${ingressInput}</p>${bodyInput}`;
 
   const body = {
     title: formData.get("title"),
-    email: formData.get("email"),
-    password: formData.get("password"),
+    body: bodyTxt,
+    media: {
+      url: formData.get("mediaUrl"),
+      alt: formData.get("mediaAlt"),
+    },
   };
+  console.log(body);
+
+  //HTTP Post request
+  try {
+    const response = await fetch(
+      `https://v2.api.noroff.dev/blog/posts/${username}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(body),
+      },
+    );
+    // API checks request
+    const data = await response.json();
+    if (!response.ok)
+      throw new Error(data.errors?.[0]?.message || "Upload failed");
+
+    // if successful: alert
+    setPopup("confirm-popup", "Your post was uploaded to the blog!", [
+      {
+        text: "Go to post",
+        class: "primary-button",
+        href: "../account/login.html",
+      },
+      { text: "Home", class: "cancel-button", href: "./" },
+    ]);
+    showPopup();
+  } catch (error) {
+    setPopup("warning-popup", error.message, [
+      { text: "back", class: "cancel-button", onClick: hidePopup },
+    ]);
+    showPopup();
+  }
 }
