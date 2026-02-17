a85dda6f58662b39c3fc4866963b8d2257ecbe40
tominehaug <tomine_haug@live.no>
2026-02-17 12:47:06 +0100

    added authentication to correct display of nav bar button

--- js/authentication.js
+++ js/authentication.js
@@ -1,7 +1,21 @@
 const isLoggedIn = Boolean(localStorage.getItem("token"));
 
-if (isLoggedIn) {
-  document.getElementById("nav-button").style.display = "none";
-} else {
-  document.getElementById("nav-button").style.display = "block";
+// Nav-bar
+
+async function authNav() {
+  const loggedOutBtn = document.querySelector(".logged-out");
+  const loggedInBtn = document.querySelector(".logged-in");
+  const username = localStorage.getItem("username");
+
+  if (isLoggedIn) {
+    loggedOutBtn.classList.add("hidden");
+    loggedInBtn.classList.remove("hidden");
+    const button = loggedInBtn.querySelector("span");
+    button.textContent = username;
+  } else {
+    loggedOutBtn.classList.remove("hidden");
+    loggedInBtn.classList.add("hidden");
+  }
 }
+
+authNav();
