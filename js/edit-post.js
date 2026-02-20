f6b726b45dce162fad22be72aa8ac277020de451
tominehaug <tomine_haug@live.no>
2026-02-20 13:28:48 +0100

    added input field for media alt

--- js/edit-post.js
+++ js/edit-post.js
@@ -27,6 +27,7 @@ async function fetchPost() {
 
 async function displayInput(post) {
   document.getElementById("mediaUrl").value = post.media.url || "";
+  document.getElementById("mediaAlt").value = post.media.alt || "";
   document.getElementById("title").value = post.title || "";
 
   // separate ingress and body text within the API body
@@ -56,20 +57,96 @@ async function init() {
 
 init();
 
-// Logic for replacing/updating the post
+// Logic for validating and replacing/updating the post
+
+const updateUrl = `https://v2.api.noroff.dev/blog/posts/${username}/${postId}`;
+const editForm = document.getElementById("edit-form");
+
+import { validateForm } from "./validation.js";
+
+editForm.addEventListener("submit", (event) => {
+  const isValid = validateForm(event, editForm);
+
+  if (isValid) {
+    updatePost();
+  }
+});
+
+async function updatePost() {
+  //Collect blog post data
+  const formData = new FormData(editForm);
+  const ingressInput = formData.get("ingress");
+  const bodyInput = formData.get("body");
+  const bodyTxt = `<h3 class="ingress">${ingressInput}</h3><p>${bodyInput}</p>`;
+
+  const updatedBody = {
+    title: formData.get("title"),
+    body: bodyTxt,
+    media: {
+      url: formData.get("mediaUrl"),
+      alt: formData.get("mediaAlt"),
+    },
+  };
+  console.log(updatedBody);
+  //HTTP Post request
+  try {
+    const response = await fetch(updateUrl, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(updatedBody),
+    });
+    // API checks request
+    const data = await response.json();
+    if (!response.ok)
+      throw new Error(data.errors?.[0]?.message || "Upload failed");
+
+    // alert: success or not
+    setPopup("confirm-popup", "Your post was uploaded to the blog!", [
+      {
+        text: "View post",
+        class: "primary-button",
+        href: `/post/index.html?id=${postId}`,
+      },
+      { text: "Home", class: "cancel-button", href: "./" },
+    ]);
+    showPopup();
+  } catch (error) {
+    setPopup("warning-popup", error.message, [
+      { text: "back", class: "cancel-button", onClick: hidePopup },
+    ]);
+    showPopup();
+  }
+}
 
 // Logic for deleting the post
 
 const deleteBtn = document.getElementById("delete-btn");
 const deleteUrl = `https://v2.api.noroff.dev/blog/posts/${username}/${postId}`;
 
-async function deletePost(url) {
+deleteBtn.addEventListener("click", () => {
+  setPopup("warning-popup", "Do you want to delete this post?", [
+    {
+      text: "Yes",
+      class: "warning-btn",
+      onclick: deletePost,
+    },
+    { text: "No", class: "cancel-button", onClick: hidePopup },
+  ]);
+  showPopup();
+});
+
+async function deletePost() {
   try {
     const deleteOptions = {
       method: "DELETE",
-      headers: `Bearer${token}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     };
-    const response = await fetch(url, deleteOptions);
+    const response = await fetch(deleteUrl, deleteOptions);
 
     const data = await response.json();
     if (!response.ok)
@@ -92,5 +169,3 @@ async function deletePost(url) {
     showPopup();
   }
 }
-
-deleteBtn.addEventListener("click", deletePost(deleteUrl));
